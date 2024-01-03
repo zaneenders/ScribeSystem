d218ec6dff286edab6dabb561c01366718bbad8e
zane <39070793+zaneenders@users.noreply.github.com>
2024-01-02 23:13:16 -0700

    files plug in

--- Plugins/FilesPlugin/FilesPlugin.swift
+++ Plugins/FilesPlugin/FilesPlugin.swift
@@ -1,6 +1,7 @@
 import Foundation
 import PackagePlugin
 
+// swift package --allow-writing-to-package-directory files
 @main
 public struct FilesPlugin: CommandPlugin {
 
@@ -10,11 +11,10 @@ public struct FilesPlugin: CommandPlugin {
         context: PluginContext,
         arguments: [String]
     ) async throws {
+        print("FilesPlugin starting")
         let swiftFormatTool = try context.tool(named: "Files")
         let swiftFormatExec = URL(fileURLWithPath: swiftFormatTool.path.string)
-        print("zane was here")
         let process = try Process.run(swiftFormatExec, arguments: arguments)
-        print(process)
         process.waitUntilExit()
     }
 }
